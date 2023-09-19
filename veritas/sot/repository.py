6f57534609b93d37ac4f04f9d3e339cedc293953
veritas-sot <veritas-sot@proton.me>
2023-09-19 14:19:08 +0200

    get commits (and details) using pydriller

--- veritas/sot/repository.py
+++ veritas/sot/repository.py
@@ -2,6 +2,7 @@ import logging
 import os
 import datetime
 from pathlib import Path
+from pydriller import Repository as pyRepository
 from git import Repo, GitCommandError
 from git.objects import Commit as GitCommit
 
@@ -69,6 +70,102 @@ class Repository:
         return (
             (commit, (commit.tree / path).data_stream.read()) for commit in self._repo.iter_commits(paths=path)
         )
+
+    def get_commits(self):
+
+        commits = []
+        for commit in pyRepository(str(self.path)).traverse_commits():
+
+            hash = commit.hash
+
+            # Gather a list of files modified in the commit
+            files = []
+            try:
+                for f in commit.modified_files:
+                    if f.new_path is not None:
+                        files.append(f.new_path) 
+            except Exception:
+                print('Could not read files for commit ' + hash)
+                continue
+
+            # Capture information about the commit in object format so I can reference it later
+            record = {
+                'hash': hash,
+                'message': commit.msg,
+                'author_name': commit.author.name,
+                'author_email': commit.author.email,
+                'author_date': str(commit.author_date),
+                'author_tz': commit.author_timezone,
+                'committer_name': commit.committer.name,
+                'committer_email': commit.committer.email,
+                'committer_date': str(commit.committer_date),
+                'committer_tz': commit.committer_timezone,
+                'in_main': commit.in_main_branch,
+                'is_merge': commit.merge,
+                'num_deletes': commit.deletions,
+                'num_inserts': commit.insertions,
+                'net_lines': commit.insertions - commit.deletions,
+                'num_files': commit.files,
+                'branches': ', '.join(commit.branches), # Comma separated list of branches the commit is found in
+                'files': ', '.join(files), # Comma separated list of files the commit modifies
+                'parents': ', '.join(commit.parents), # Comma separated list of parents
+                # PyDriller Open Source Delta Maintainability Model (OS-DMM) stat. See https://pydriller.readthedocs.io/en/latest/deltamaintainability.html for metric definitions
+                'dmm_unit_size': commit.dmm_unit_size,
+                'dmm_unit_complexity': commit.dmm_unit_complexity,
+                'dmm_unit_interfacing': commit.dmm_unit_interfacing,
+            }
+            # Omitted: modified_files (list), project_path, project_name
+            commits.append(record)
+        return commits
+
+    def get_commits_details(self, diff=False, diff_parsed=False, source=False, source_before=False):
+        commits = []
+
+        for commit in pyRepository(str(self.path)).traverse_commits():
+            hash = commit.hash
+            try:
+                for f in commit.modified_files:
+                    record = {
+                        'hash': hash,
+                        'message': commit.msg,
+                        'author_name': commit.author.name,
+                        'author_email': commit.author.email,
+                        'author_date': str(commit.author_date),
+                        'author_tz': commit.author_timezone,
+                        'committer_name': commit.committer.name,
+                        'committer_email': commit.committer.email,
+                        'committer_date': str(commit.committer_date),
+                        'committer_tz': commit.committer_timezone,
+                        'in_main': commit.in_main_branch,
+                        'is_merge': commit.merge,
+                        'num_deletes': commit.deletions,
+                        'num_inserts': commit.insertions,
+                        'net_lines': commit.insertions - commit.deletions,
+                        'num_files': commit.files,
+                        'branches': ', '.join(commit.branches),
+                        'filename': f.filename,
+                        'old_path': f.old_path,
+                        'new_path': f.new_path,
+                        'nloc': f.nloc,
+                        'added_lines': f.added_lines,
+                        'deleted_lines': f.deleted_lines,
+                        'project_name': commit.project_name,
+                        'project_path': commit.project_path, 
+                        'parents': ', '.join(commit.parents),
+                    }
+                    if diff:
+                        record.update({'diff': f.diff})
+                    if diff_parsed:
+                        record.update({'diff_parsed': f.diff_parsed})
+                    if source:
+                        record.update({'source_code': f.source_code})
+                    if source_before:
+                        record.update({'source_code_before': f.source_code_before})
+                    commits.append(record)
+            except Exception:
+                print('Problem reading commit ' + hash)
+                continue 
+        return commits
 
     def set_config(self, key, sub_ey, value):
         # user.name = value
