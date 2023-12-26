c750b4434844aff40376b11b909854930cd569a8
veritas-sot <veritas-sot@proton.me>
2023-12-26 16:16:18 +0100

    implement queue to write to database

--- veritas/messagebus/messagebus.py
+++ veritas/messagebus/messagebus.py
@@ -1,20 +1,22 @@
 import psycopg2
 import zmq
+import atexit
 from zmq.log.handlers import PUBHandler
 from loguru import logger
+from threading import Thread
+from queue import Queue
 
 
 class Messagebus():
 
-    def __init__(self, 
-                 app=None, uuid=None,
-                 database=None,
-                 zeromq=None):
+    def __init__(self, app=None, uuid=None, database=None, zeromq=None, use_queue=False):
         """init veritas messagesbus"""
 
         # general
         self.__app_name = app
         self.__uuid = uuid
+        self.__queue = Queue()
+        self.__use_queue = use_queue
 
         # database
         self._database = None
@@ -24,6 +26,14 @@ class Messagebus():
         if database:
             self._database = database
             self._connect_to_db()
+            if use_queue:
+                # call exit handler to empty queue
+                atexit.register(self._empty_queue)
+
+                self._consumer = Thread(target=self._dequeue,
+                                        args=(self.__queue,))
+                self._consumer.daemon = True
+                self._consumer.start()
 
         # zeroMQ
         if zeromq:
@@ -34,11 +44,12 @@ class Messagebus():
             socket = zmq.Context().socket(zmq.PUB)
             socket.connect(f'{protocol}://{host}:{port}')
             zmq_handler = PUBHandler(socket)
-            logger.add(zmq_handler)
+            logger.add(zmq_handler, 
+                       filter=self._zeromq_filter,
+                       serialize=True)
 
         # list of sinks
         self._sinks = []
-
 
     def write(self, message):
         record = message.record
@@ -53,21 +64,31 @@ class Messagebus():
         processname = record['process'].name
         threadname = record['thread'].name
         exception = record['exception']
+        extra = record['extra']
 
         if self._database:
-            self._write_to_database(levelno, 
-                                    levelname, 
-                                    message, 
-                                    filename, 
-                                    pathname, 
-                                    lineno, 
-                                    module, 
-                                    function, 
-                                    processname, 
-                                    threadname, 
-                                    exception)
+            if self.__use_queue:
+                self.__queue.put(record)
+            else:
+                self._execute_sql(levelno, 
+                                  levelname, 
+                                  message, 
+                                  filename, 
+                                  pathname, 
+                                  lineno, 
+                                  module, 
+                                  function, 
+                                  processname, 
+                                  threadname, 
+                                  exception)
 
     # internals
+
+    def _zeromq_filter(self, record):
+        extra = record['extra']
+        if extra.get('zeromq', False):
+            return True
+        return False
 
     def _connect_to_db(self):
         """connet to database"""
@@ -80,7 +101,7 @@ class Messagebus():
 
         self._cursor = self._db_connection.cursor()
 
-    def _write_to_database(self, levelno, levelname, message, filename, 
+    def _execute_sql(self, levelno, levelname, message, filename, 
                             pathname, lineno, module, function, 
                             processname, threadname, exception):
         if self.__uuid:
@@ -122,5 +143,42 @@ class Messagebus():
         # commit the changes to the database
         self._db_connection.commit()
 
-
+    def _dequeue(self, queue):
+        while True:
+            record = queue.get()
+            self._record_to_database(record)
+            # check for stop
+            if record is None:
+                break
     
+    def _record_to_database(self, record):
+            # write to database
+            levelno = record['level'].no
+            levelname = record['level'].name
+            message = record['message']
+            filename = record['file'].name
+            pathname = record['file'].path
+            lineno = record['line']
+            module = record['module']
+            function = record['function']
+            processname = record['process'].name
+            threadname = record['thread'].name
+            exception = record['exception']
+            extra = record['extra']
+
+            self._execute_sql(levelno, 
+                              levelname, 
+                              message, 
+                              filename, 
+                              pathname, 
+                              lineno, 
+                              module, 
+                              function, 
+                              processname, 
+                              threadname, 
+                              exception)
+
+    def _empty_queue(self):
+        while self.__queue.qsize() > 0:
+            record = queue.get()
+            self._record_to_database(record)
